0a65be9edbc8861a9d7201e32436a4cee597bbdc
Lin Lin <lin2013lin@gmail.com>
2023-03-29 19:06:34 -0400

    updated seeds

--- seeds/users.js
+++ seeds/users.js
@@ -14,10 +14,7 @@ exports.up = function (knex) {
   });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.down = function (knex) {
   return knex.schema.dropTable('warehouses');
 };
